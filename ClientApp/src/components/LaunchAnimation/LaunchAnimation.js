740bb6d96d517b8d479daa236c110c982e408e0c
unknown <gary.moore907@gmail.com>
2020-04-19 16:22:41 -0600

    Get animation working

--- ClientApp/src/components/LaunchAnimation/LaunchAnimation.js
+++ ClientApp/src/components/LaunchAnimation/LaunchAnimation.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useSpring, animated } from 'react-spring';
+
 import './styles.css';
 
 const LaunchAnimation = ({ mass, angle, force }) => (
   <div className="LaunchAnimation">
     <div className="title">Launch Animation</div>
     <div>
-      {mass}
-      {angle}
-      {force}
+      <animated.div style={useSpring({ opacity: 1, from: { opacity: 0 } })}>I will fade in</animated.div>
     </div>
   </div>
 );
