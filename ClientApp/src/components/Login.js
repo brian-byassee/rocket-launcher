a5f7799d99755a71264b4b3740d6f8a190189fc5
Brian <bbyassee10@gmail.com>
2020-04-19 14:47:50 -0600

    working on login

--- ClientApp/src/components/Login.js
+++ ClientApp/src/components/Login.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
-const Login = () => (<div>Hello from login component</div>);
+const Login = () => {
+  async getLogin => {
+    const response = await fetch('login');
+    const data = await response.json();
+    this.setState({ forecasts: data, loading: false });
+  };
+  
+  return (<div>Hello from login component</div>)
+
+};
 
 export default Login;
